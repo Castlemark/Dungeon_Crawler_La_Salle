3ba08a109299800745d592e687572f292c09c0c9
pnonell <pau.nonell@gmail.com>
2018-01-07 19:12:50 +0100

    mostrar nivell de mapa

--- src/js/move.js
+++ src/js/move.js
@@ -163,6 +163,19 @@ function actualizarMapa(){
     default:
     break;
   }
+
+  //poner nivel de mapa
+  switch(partida.jugador.posicion.mapa){
+    case 0:
+    $('#nivel-actual').html('Nivel -3');
+    break;
+    case 1:
+    $('#nivel-actual').html('Nivel -2');
+    break;
+    case 2:
+    $('#nivel-actual').html('Nivel -1');
+    break;
+  }
 }
 
 function comprovarPosicion(){
